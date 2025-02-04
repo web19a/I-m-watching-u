72c175e67b75194d355d283f9921b8b419b0b2a4
web19a <bura.anan19@gmail.com>
2025-02-04 19:13:51 +0300

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-document.addEventListener('mousemove', (event) => {
+function movePupils(x, y) {
     const eyes = document.querySelectorAll('.eye');
     eyes.forEach(eye => {
         const pupil = eye.querySelector('.pupil');
         const rect = eye.getBoundingClientRect();
         const eyeX = rect.left + rect.width / 2;
         const eyeY = rect.top + rect.height / 2;
-        const angle = Math.atan2(event.pageY - eyeY, event.pageX - eyeX);
-        const pupilX = Math.cos(angle) * (rect.width / 4);
-        const pupilY = Math.sin(angle) * (rect.height / 4);
-        pupil.style.transform = `translate(${pupilX}px, ${pupilY}px)`;
+        const angle = Math.atan2(y - eyeY, x - eyeX);
+        const distance = Math.min(rect.width / 2 - pupil.offsetWidth / 2 - 6, Math.hypot(x - eyeX, y - eyeY));
+        const constrainedX = distance * Math.cos(angle);
+        const constrainedY = distance * Math.sin(angle);
+        pupil.style.transform = `translate(${constrainedX}px, ${constrainedY}px) translate(-50%, -50%)`;
     });
+}
+
+const hangl = document.getElementById('handl')
+const hangr = document.getElementById('handr')
+
+document.addEventListener('mousemove', function (e) {
+    movePupils(e.pageX, e.pageY);
+});
+
+document.addEventListener('touchmove', function (e) {
+    const touch = e.touches[0];
+    movePupils(touch.pageX, touch.pageY);
 });
